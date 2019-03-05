e2d4d9432daf11743ee53d0c4cc1e996f92afb7b
Justine B <justinegayton@gmail.com>
2019-03-05 19:53:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-function Scooter(year, color, model) {console.log(this)}function driver(name, age, experience) {console.log(this)}function Scooter(name, age, hometown) {console.log(this)}
+function Scooter(year, color, model) {console.log(this)}function Driver(name, age, experience) {console.log(this)}function Scooter(name, age, hometown) {console.log(this)}
